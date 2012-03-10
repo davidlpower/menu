46baf63b14e7619f1466b39728617472c1254e63
David Power <david.lpower@gmail.com>
2012-03-10 23:23:22 +0000

    update fonts

--- application/views/post/index.php
+++ application/views/post/index.php
@@ -25,8 +25,9 @@
 
         <h2><?php echo $post->title; ?></h2>
         <pre><?php echo $post->content; ?></pre>
-        <div class="button"><?php echo HTML::anchor("post/edit/" . $post->id, "Edit"); ?></div>
-        <div class="button"><?php echo HTML::anchor("post/delete/" . $post->id, "Delete"); ?></div>
+        <?php echo HTML::anchor("post/edit/" . $post->id, "Edit"); ?>
+        <?php echo "  "; ?>
+        <?php echo HTML::anchor("post/delete/" . $post->id, "Delete"); ?>
 
     </div>
 
