7adef906c2124fca80a6c8407a3b895023e97cd5
David Power <david.lpower@gmail.com>
2012-03-10 23:51:03 +0000

    update fonts

--- application/views/post/index.php
+++ application/views/post/index.php
@@ -22,11 +22,11 @@
     <?php foreach ($postItems as $post) : ?>
 
     <div class="menuItem">
-
         <h2><?php echo $post->title; ?></h2>
         <?php echo $post->content; ?>
+        <br />
         <?php echo HTML::anchor("post/edit/" . $post->id, "Edit"); ?>
-        <?php echo "  "; ?>
+        <?php echo " | "; ?>
         <?php echo HTML::anchor("post/delete/" . $post->id, "Delete"); ?>
 
     </div>
