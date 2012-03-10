6ae4c31445d5e3668484f8d26f9144d5d351db2a
David Power <david.lpower@gmail.com>
2012-03-10 23:06:16 +0000

    update fonts

--- application/views/post/headder.php
+++ application/views/post/headder.php
@@ -4,7 +4,7 @@
     <title>David Power's Online Portfolio</title>
 <?php
     echo html::style("media/css/stylesheet.css");
-    echo html::style("<link href='http://fonts.googleapis.com/css?family=Prosto+One' rel='stylesheet' type='text/css'>");
+    echo html::style("<link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>");
     echo html::style("<link href='http://fonts.googleapis.com/css?family=Exo' rel='stylesheet' type='text/css'>");
     echo html::style("<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two' rel='stylesheet' type='text/css'>");
 ?>
