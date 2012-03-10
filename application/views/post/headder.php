96fa6ac1f4755f32c6f7844d786330d01b95f55a
David Power <david.lpower@gmail.com>
2012-03-10 22:16:08 +0000

    Updated edit

--- application/views/post/headder.php
+++ application/views/post/headder.php
@@ -6,6 +6,7 @@
     echo html::style("media/css/stylesheet.css");
     echo html::style("<link href='http://fonts.googleapis.com/css?family=PT+Mono' rel='stylesheet' type='text/css'>");
     echo html::style("<link href='http://fonts.googleapis.com/css?family=Exo' rel='stylesheet' type='text/css'>");
+    echo html::style("<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two' rel='stylesheet' type='text/css'>");
 ?>
     </HEAD>
 
