932e1c0999aa9fb0545a88d369e2df874153e4e3
David Power <david.lpower@gmail.com>
2012-03-10 21:45:42 +0000

    updated the style with new font

--- application/views/post/headder.php
+++ application/views/post/headder.php
@@ -4,6 +4,7 @@
     <title>David Power's Online Portfolio</title>
 <?php
     echo html::style("media/css/stylesheet.css");
+    echo html::style("<link href='http://fonts.googleapis.com/css?family=PT+Mono' rel='stylesheet' type='text/css'>");
 ?>
     </HEAD>
 
