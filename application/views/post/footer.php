c6e8d91d858dc3c51b7e7f692b4a999d2a89d291
David Power <david.lpower@gmail.com>
2012-03-11 02:50:30 +0000

    Facebook alignment

--- application/views/post/footer.php
+++ application/views/post/footer.php
@@ -1,6 +1,5 @@
 <div class="footer">
-&copy;David Power &nbsp;
-<div class="fb-like" data-href="http://www.karujahundu.com/" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-font="segoe ui"></div>
+<div class="fb-like" data-href="http://www.karujahundu.com/" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-font="segoe ui">&copy;David Power &nbsp;</div>
 </div>
 </BODY>
 </HTML>
