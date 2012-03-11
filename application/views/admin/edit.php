c3fdce3ead7e34701d905ca16951a1a32011c06e
David Power <david.lpower@gmail.com>
2012-03-11 22:17:00 +0000

    update edit screen

--- application/views/admin/edit.php
+++ application/views/admin/edit.php
@@ -8,26 +8,26 @@ tinyMCE.init({
 });
 </script>
 
-
-
 <div id="container">
     <div id="title">
         <div class="centerTitle">
             <h1>Edit or Post new stuff!</h1>
         </div>
     </div>
-    <br/>
-    <?php echo Form::open('admin/post/' . $post->id); ?>
-    <?php echo Form::label("title", "Title"); ?>
-    <br/>
-    <?php echo Form::input("title", $post->title, $settings=array("size='100'","style='height:35px; font-size: large;'")); ?>
-    <br/>
-    <br/>
-    <?php echo Form::label("content", "Content"); ?>
-    <br/>
-    <?php echo Form::textarea("content", $post->content, $settings=array("style='WIDTH: 600px; HEIGHT: 400px'")); ?>
-    <br/>
-    <br/>
-    <?php echo Form::submit("submit", "Submit"); ?>
-    <?php echo Form::close(); ?>
+    <div class="centerTitle">
+        <br/>
+        <?php echo Form::open('admin/post/' . $post->id); ?>
+        <?php echo Form::label("title", "Title"); ?>
+        <br/>
+        <?php echo Form::input("title", $post->title, $settings=array("size='100'","style='height:35px; font-size: large;'")); ?>
+        <br/>
+        <br/>
+        <?php echo Form::label("content", "Content"); ?>
+        <br/>
+        <?php echo Form::textarea("content", $post->content, $settings=array("style='WIDTH: 650px; HEIGHT: 400px'")); ?>
+        <br/>
+        <br/>
+        <?php echo Form::submit("submit", "Submit"); ?>
+        <?php echo Form::close(); ?>
+    </div>
 </div>
