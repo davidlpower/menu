f7f200a558f951d218745a3b55bdde3a36c73cc7
David Power <david.lpower@gmail.com>
2012-03-11 02:14:18 +0000

    Add TinyMCE to edit page

--- application/views/post/edit.php
+++ application/views/post/edit.php
@@ -3,7 +3,7 @@ defined('SYSPATH') or die('No direct script access.');
 include 'headder.php';
 ?>
 
-<script type="text/javascript" src="media/tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
+<script type="text/javascript" src="/media/tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
 <script type="text/javascript">
 tinyMCE.init({
         mode : "textareas"
@@ -33,14 +33,6 @@ tinyMCE.init({
     <?php echo Form::submit("submit", "Submit"); ?>
     <?php echo Form::close(); ?>
 </div>
-
-
-<form method="post" action="show.php">
-        <p>     
-                <textarea name="content" cols="50" rows="15">This is some content that will be editable with TinyMCE.</textarea>
-                <input type="submit" value="Save" />
-        </p>
-</form>
 
 <?php
 include 'footer.php';
