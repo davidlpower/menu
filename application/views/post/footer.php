5378765d4da0eff22bdfeaa5d9509b8a9bb87bb4
David Power <david.lpower@gmail.com>
2012-03-11 02:33:38 +0000

    Facebook Button

--- application/views/post/footer.php
+++ application/views/post/footer.php
@@ -1,6 +1,8 @@
 <div class="footer">
-&copy;David Power
+<p>
+&copy;David Power &nbsp;
 <div class="fb-like" data-href="http://www.karujahundu.com/" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-font="segoe ui"></div>
+</p>
 </div>
 </BODY>
 </HTML>
