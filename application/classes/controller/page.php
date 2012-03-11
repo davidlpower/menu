175bc2dc6e865a18f33b35b1f605064dbedebee8
David Power <david.lpower@gmail.com>
2012-03-11 14:40:31 +0000

    Trying to expose the Temp class to the controller

--- application/classes/controller/page.php
+++ application/classes/controller/page.php
@@ -1,18 +1,16 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-  
-  class Controller_Page extends Controller_Temp {
-  
-  	public function action_home()
-  	{
-  		$this->template->title = __('Software, Electronics, Music and all-round Geekery');
-  		
-  		$this->template->content = View::factory('post/home' );
-  	}
+
+class Controller_Page extends Controller_Temp {
+
+    public function action_home()
+    {
+        $this->template->title = __('Software, Electronics, Music and all-round Geekery');
   	
-  	public function action_contact()
-  	{
-  		$this->template->title = __('Contact page');
-  		
-  		$this->template->content = View::factory('post/contact' );
-  	}
+  	$this->template->content = View::factory('post/home' );
+    }
+    public function action_contact()
+    {
+        $this->template->title = __('Contact page');
+  	$this->template->content = View::factory('post/contact' );
+    }
 } 
