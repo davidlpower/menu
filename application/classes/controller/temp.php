ae8c87748b35aee216f0e0680067364bfb692be5
David Power <david.lpower@gmail.com>
2012-03-11 14:42:49 +0000

    Trying to expose the Temp class to the controller

--- application/classes/controller/temp.php
+++ application/classes/controller/temp.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
   
-  class Controller_Tempsite extends Controller_Template 
+  class Controller_Temp extends Controller_Template 
   {
   
       public $template = 'demo/template';
