77b251b355cc55b3b05aea4b4b9f5837f1b651ac
David Power <david.lpower@gmail.com>
2012-03-11 00:27:35 +0000

    major index update

--- application/views/post/headder.php
+++ application/views/post/headder.php
@@ -1,7 +1,7 @@
 <HTML>
 
 <HEAD>
-    <title>David Power's Online Portfolio</title>
+    <title></title>
 <?php
     echo html::style("media/css/stylesheet.css");
     echo html::style("<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>");   
