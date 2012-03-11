79d782063079525402f45a200c48a1368ff91a8b
David Power <david.lpower@gmail.com>
2012-03-11 02:27:24 +0000

    Add TinyMCE to edit page

--- application/views/post/footer.php
+++ application/views/post/footer.php
@@ -1,5 +1,12 @@
 <div class="footer">
-<p>&copy;David Power</p>
+<p>&copy;David Power</p><p><div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script></p>
 </div>
 </BODY>
 </HTML>
