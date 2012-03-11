c5f57a19d9b853f7c74df09a7765bab463dcb015
David Power <david.lpower@gmail.com>
2012-03-11 02:29:47 +0000

    Add TinyMCE to edit page

--- application/views/post/footer.php
+++ application/views/post/footer.php
@@ -1,12 +1,6 @@
 <div class="footer">
-<p>&copy;David Power</p><p><div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script></p>
+<p>&copy;David Power</p>
+<div class="fb-like" data-href="http://www.karujahundu.com/" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-font="segoe ui"></div>
 </div>
 </BODY>
 </HTML>
