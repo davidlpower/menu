679cd8ac444dbcb4835101cddfa52e13e0573921
David Power <david.lpower@gmail.com>
2012-03-11 16:26:00 +0000

    Updating the template title var

--- application/classes/controller/post.php
+++ application/classes/controller/post.php
@@ -21,7 +21,7 @@ class Controller_Post extends Controller_Temp
             $view->set("restaurantItems", $restaurantItems); // set/send "restaurantItems" object to view
         }
         */
-        $aTitle =  __('Software, Electronics, Music and all-round Geekery');
+        $aTitle = 'Software, Electronics, Music and all-round Geekery';
         $this->template->title = View::factory($aTitle);
         $this->template->content = View::factory('post/index');
         $this->template->postItems = $postItems;
