8f4b67e736381114282459743efb6503df495abc
David Power <david.lpower@gmail.com>
2012-03-11 20:09:35 +0000

    adding admin page

--- application/classes/controller/admin.php
+++ application/classes/controller/admin.php
@@ -29,21 +29,21 @@ class Controller_Admin extends Controller_Temp
     // loads the new article form
     public function action_new()
     {
-        $admin = new Model_admin();
+        $post = new Model_post();
         
         $aTitle = 'Edit or Post something new!';
         $this->template->title = View::bind_global('title', $aTitle);
         $this->template->content = View::factory('admin/edit');
-        $this->template->admin = View::bind_global('admin',$admin);
+        $this->template->post = View::bind_global('post',$post);
     }
 
     // save the article
     public function action_post()
     {
-        $admin_id = $this->request->param('id');
-        $admin = new Model_admin($admin_id);
-        $admin->values($_admin); // populate $admin object from $_admin array
-        $admin->save(); // saves admin to database
+        $post_id = $this->request->param('id');
+        $post = new Model_post($post_id);
+        $post->values($post); // populate $post object from $_post array
+        $post->save(); // saves post to database
 
         $this->request->redirect('/admin'); // redirects to admin page after saving
     }
@@ -51,21 +51,21 @@ class Controller_Admin extends Controller_Temp
     // edit the admin items
     public function action_edit()
     {
-        $admin_id = $this->request->param('id');
-        $admin = new Model_admin($admin_id);
-        $aTitle = 'Edit that admin!';
+        $post_id = $this->request->param('id');
+        $post = new Model_post($post_id);
+        $aTitle = 'Edit that post!';
         $this->template->title = View::bind_global('title', $aTitle);
-        $this->template->content = View::factory('admin/edit');
-        $this->template->admin = View::bind_global('admin',$admin);
+        $this->template->content = View::factory('post/edit');
+        $this->template->post = View::bind_global('post',$post);
     }
 
     // delete the post item
     public function action_delete()
     {
-        $admin_id = $this->request->param('id');
-        $admin = new Model_admin($admin_id);
+        $post_id = $this->request->param('id');
+        $post = new Model_post($post_id);
 
-        $admin->delete(); // delete in database
+        $post->delete(); // delete in database
         $this->request->redirect(self::INDEX_PAGE);
     }
-} // End admin
+} // End post
