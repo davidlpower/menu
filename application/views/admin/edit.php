6347c1ddf1b4981c0be1ac96ca8f36df5bb44f20
David Power <david.lpower@gmail.com>
2012-03-11 22:27:31 +0000

    update edit screen

--- application/views/admin/edit.php
+++ application/views/admin/edit.php
@@ -18,13 +18,13 @@ tinyMCE.init({
         <br/>
         <?php echo Form::open('admin/post/' . $post->id); ?>
         <?php echo Form::label("title", "Title: "); ?>
-        <?php echo Form::input("title", $post->title, $settings=array("size='100'","style='height:35px; font-size: large;'")); ?>
+        <?php echo Form::input("title", $post->title, $settings=array("size='80'","style='height:35px; font-size: large;'")); ?>
         <br/>
         <br/>
         <div class="floatLeft">
             <?php echo Form::label("content", "Content"); ?>
         </div>
-        <?php echo Form::textarea("content", $post->content, $settings=array("style='WIDTH: 650px; HEIGHT: 400px'")); ?>
+        <?php echo Form::textarea("content", $post->content, $settings=array("style='WIDTH: 550px; HEIGHT: 400px'")); ?>
         <br/>
         <br/>
         <?php echo Form::submit("submit", "Submit"); ?>
