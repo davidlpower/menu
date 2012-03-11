29666c783b3703ad678d872ec2ca54301e899471
David Power <david.lpower@gmail.com>
2012-03-11 15:03:34 +0000

    It works now. Going to try get it to produce my current content

--- application/classes/controller/temp.php
+++ application/classes/controller/temp.php
@@ -1,56 +1,56 @@
 <?php defined('SYSPATH') or die('No direct script access.');
   
-  class Controller_Temp extends Controller_Template 
-  {
-  
-      public $template = 'template/template';
-  
-      /**
-       * The before() method is called before your controller action.
-       * In our template controller we override this method so that we can
-       * set up default values. These variables are then available to our
-       * controllers if they need to be modified.
-       */
-      
-      public function before()
-      {
-          parent::before();
-  
-  	    if ($this->auto_render)
-  	    {
-  	    	// Initialize empty values
-  	    	$this->template->title   = '';
-  	    	$this->template->content = '';
-  			
-  		$this->template->styles = array();
-  		$this->template->scripts = array();
-          			
-          }
-      }
-  	
-      /**
-       * The after() method is called after your controller action.
-       * In our template controller we override this method so that we can
-       * make any last minute modifications to the template before anything
-       * is rendered.
-       */
-      
-      public function after()
-      {
-	if ($this->auto_render)
-	{
-		$styles = array(
-			'<link href=\'http://fonts.googleapis.com/css?family=Open+Sans\' rel=\'stylesheet\' type=\'text/css\'>' => 'print',
-			'media/css/stylesheet.css' => 'screen',
-		);
-  
-		$scripts = array(
-			'',
-		);
-		
-		$this->template->styles = array_merge( $this->template->styles, $styles );
-		$this->template->scripts = array_merge( $this->template->scripts, $scripts );
-	}
-	parent::after();
+class Controller_Temp extends Controller_Template 
+{
+
+    public $template = 'template/template';
+
+    /**
+    * The before() method is called before your controller action.
+    * In our template controller we override this method so that we can
+    * set up default values. These variables are then available to our
+    * controllers if they need to be modified.
+    */
+
+    public function before()
+    {
+        parent::before();
+
+        if ($this->auto_render)
+        {
+            // Initialize empty values
+            $this->template->title   = '';
+            $this->template->content = '';
+
+            $this->template->styles = array();
+            $this->template->scripts = array();
+
+        }
+    }
+
+    /**
+    * The after() method is called after your controller action.
+    * In our template controller we override this method so that we can
+    * make any last minute modifications to the template before anything
+    * is rendered.
+    */
+
+    public function after()
+    {
+    if ($this->auto_render)
+    {
+            $styles = array(
+                    '<link href=\'http://fonts.googleapis.com/css?family=Open+Sans\' rel=\'stylesheet\' type=\'text/css\'>' => 'print',
+                    'media/css/stylesheet.css' => 'screen',
+            );
+
+            $scripts = array(
+                    '',
+            );
+
+            $this->template->styles = array_merge( $this->template->styles, $styles );
+            $this->template->scripts = array_merge( $this->template->scripts, $scripts );
+    }
+    parent::after();
     }
 }
