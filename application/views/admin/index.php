a81c20ae66423760255fd73d9ae8de6413c5b5db
David Power <david.lpower@gmail.com>
2012-03-15 22:19:22 +0000

    Apply the bootstrap

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -7,22 +7,7 @@
         </div>
 
         <!--Display new menu-->
-        <div id="menuBar">
-            <div class="centerTitle">
-                <!-- Add a link to the post page. -->
-                <?php echo HTML::anchor("admin/new", "New Post"); ?>
-                <?php echo " | " ?>
-                <?php echo HTML::anchor("admin/", "Software"); ?>
-                <?php echo " | " ?>
-                <?php echo HTML::anchor("admin/", "Electronics"); ?>
-                <?php echo " | " ?>
-                <?php echo HTML::anchor("admin/", "Music"); ?>
-                <?php echo " | " ?>
-                <?php echo HTML::anchor("admin/", "Everything Else"); ?>
-                <?php echo " | " ?>
-                <?php echo HTML::anchor("post/", "Contact"); ?>
-            </div>
-        </div>
+        <?php include 'application/views/template/menuBar.php';?>
 
     </div>
 
