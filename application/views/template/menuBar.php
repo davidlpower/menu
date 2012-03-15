548f3968fd51409e8d135fa4d6db747954933a26
David Power <david.lpower@gmail.com>
2012-03-15 22:49:28 +0000

    Apply the bootstrap

--- application/views/template/menuBar.php
+++ application/views/template/menuBar.php
@@ -3,11 +3,11 @@
     <div class="centerTitle">
         <ul class="nav nav-pills">
             <li><a href='<?php echo HTML::anchor("admin/new") ?>'>New Post</a></li>
-            <li><a href="<?php echo HTML::anchor('admin/software'); ?>Software</a></li>
-            <li><a href="<?php echo HTML::anchor('admin/electronics'); ?>Electronics</a></li>
-            <li><a href="<?php echo HTML::anchor('admin/music'); ?>Music</a></li>
-            <li><a href="<?php echo HTML::anchor('admin/everything_else'); ?>Everything Else</a></li>
-            <li><a href="<?php echo HTML::anchor('admin/contact'); ?>Contact</a></li>
+            <li><a href='<?php echo HTML::anchor("admin/software") ?>'>Software</a></li>
+            <li><a href='<?php echo HTML::anchor("admin/electronics") ?>'>Electronics</a></li>            
+            <li><a href='<?php echo HTML::anchor("admin/music") ?>'>Music</a></li>
+            <li><a href='<?php echo HTML::anchor("admin/everything_else") ?>'>Everything Else</a></li>
+            <li><a href='<?php echo HTML::anchor("admin/contact") ?>'>Contact</a></li>
         </ul>
     </div>
 </div>
