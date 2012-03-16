b0a878fbb416b1f3f57b791b847df10b9ede317a
David Power <david.lpower@gmail.com>
2012-03-16 22:35:22 +0000

    removing the Twitter bootstrap CSS

--- application/views/template/menuBar.php
+++ application/views/template/menuBar.php
@@ -1,7 +1,7 @@
 <!--Display new menu-->
 <div id="menuBar">
     <div class="centerTitle">
-        <a href='<?php echo HTML::anchor("admin/new", New Post) ?>'>New Post</a>
+        <a href='<?php echo HTML::anchor("admin/new") ?>'>New Post</a>
         <a href='<?php echo HTML::anchor("admin/software") ?>'>Software</a>
         <a href='<?php echo HTML::anchor("admin/electronics") ?>'>Electronics</a>            
         <a href='<?php echo HTML::anchor("admin/music") ?>'>Music</a>
