d61b9950677cb0d3ee9f0b4e92e5e98f267a42a8
David Power <david.lpower@gmail.com>
2012-03-16 22:29:11 +0000

    removing the Twitter bootstrap

--- application/views/template/menuBar.php
+++ application/views/template/menuBar.php
@@ -1,13 +1,11 @@
 <!--Display new menu-->
 <div id="menuBar">
     <div class="centerTitle">
-        <ul class="nav nav-pills">
-            <li><a href='<?php echo HTML::anchor("admin/new") ?>'>New Post</a></li>
-            <li><a href='<?php echo HTML::anchor("admin/software") ?>'>Software</a></li>
-            <li><a href='<?php echo HTML::anchor("admin/electronics") ?>'>Electronics</a></li>            
-            <li><a href='<?php echo HTML::anchor("admin/music") ?>'>Music</a></li>
-            <li><a href='<?php echo HTML::anchor("admin/everything_else") ?>'>Everything Else</a></li>
-            <li><a href='<?php echo HTML::anchor("admin/contact") ?>'>Contact</a></li>
-        </ul>
+        <a href='<?php echo HTML::anchor("admin/new", New Post) ?>'>New Post</a>
+        <a href='<?php echo HTML::anchor("admin/software") ?>'>Software</a>
+        <a href='<?php echo HTML::anchor("admin/electronics") ?>'>Electronics</a>            
+        <a href='<?php echo HTML::anchor("admin/music") ?>'>Music</a>
+        <a href='<?php echo HTML::anchor("admin/everything_else") ?>'>Everything Else</a>
+        <a href='<?php echo HTML::anchor("admin/contact") ?>'>Contact</a>
     </div>
 </div>
