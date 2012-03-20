b5352e30811b029e936c76f51a84a49c95d89f8d
David Power <david.lpower@gmail.com>
2012-03-20 22:35:19 +0000

    adding a better 404 page

--- application/classes/controller/errors.php
+++ application/classes/controller/errors.php
@@ -4,7 +4,7 @@ class Controller_Errors extends Controller_Temp {
 
     public function action_404() {
         
-        $title = '404 !';
+        $title = 'Oops!';
         $this->request->status = 404;
         $this->template->title = View::bind_global('title', $title);
         $this->template->title = View::bind_global('site_title', $title);
--- application/views/errors/404.php
+++ application/views/errors/404.php
@@ -6,5 +6,10 @@
             <h1><?php echo($title) ?></h1>
         </div>
     </div>
-    <center><h1>Oops! an error has occurred!</h1></center>
+    <center>
+        <h1>What have you done?!</h1>
+        <p>I suggest you start again 
+        <?php echo HTML::anchor('/','Click Me!') ?>
+        </p>
+    </center>
 </div>
