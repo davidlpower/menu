f0115b5502f80697bdd08153b0b7879f6c385a36
David Power <david.lpower@gmail.com>
2012-03-20 22:23:04 +0000

    adding new bootstrap rules

--- application/classes/controller/errors.php
+++ application/classes/controller/errors.php
@@ -3,9 +3,11 @@
 class Controller_Errors extends Controller_Temp {
 
     public function action_404() {
+        
+        $title = '404 !';
         $this->request->status = 404;
-        $this->template->title = View::bind_global('title', '404 !');
-        $this->template->title = View::bind_global('site_title', '404 Oops!');
+        $this->template->title = View::bind_global('title', $title);
+        $this->template->title = View::bind_global('site_title', $title);
         $this->template->content = View::factory('errors/404');
     }
 }
