4f71da3052d4b4a8628d9ef40ae0ea0f9a8d9dbd
David Power <david.lpower@gmail.com>
2012-03-20 21:57:31 +0000

    Finished editing the menu bar

--- application/views/template/menuBar.php
+++ application/views/template/menuBar.php
@@ -3,16 +3,14 @@
     <div class="centerTitle">
         <?php echo HTML::anchor('admin/new','New Post') ?>
         <?php echo " | " ?>
-        <a href="<?php echo HTML::anchor('admin/software') ?>"Software</a>
+        <?php echo HTML::anchor('admin/software','Software') ?>
         <?php echo " | " ?>
-        <a href='<?php echo HTML::anchor("admin/electronics") ?>'>Electronics</a>  
+        <?php echo HTML::anchor('admin/electronics','Electronics') ?>
         <?php echo " | " ?>
-        <a href='<?php echo HTML::anchor("admin/music") ?>'>Music</a>
+        <?php echo HTML::anchor('admin/music','Music') ?>
         <?php echo " | " ?>
-        <a href='<?php echo HTML::anchor("admin/cooking") ?>'>Cooking</a>
+        <?php echo HTML::anchor('admin/cooking','Cooking') ?>
         <?php echo " | " ?>
-        <a href='<?php echo HTML::anchor("admin/everything_else") ?>'>Everything Else</a>
-        <?php echo " | " ?>
-        <a href='<?php echo HTML::anchor("admin/contact") ?>'>Contact</a>
+        <?php echo HTML::anchor('admin/everything_else','Everything Else') ?>
     </div>
 </div>
