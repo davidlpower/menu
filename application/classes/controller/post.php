f6656b55b2d36fe5c283954d75be6dc99bbef5eb
David Power <david.lpower@gmail.com>
2012-03-20 23:10:25 +0000

    Removing post_types and introducing post types in post

--- application/classes/controller/post.php
+++ application/classes/controller/post.php
@@ -1,32 +1,73 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php
 
-class Controller_Post extends Controller_Temp
-{
+defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Post extends Controller_Temp {
+
     const INDEX_PAGE = '/';
+
     public $mobileDevice = null;
 
-    public function action_index()
-    {
-        $postItems = ORM::factory('post')->find_all(); // load all post object from table
+    public function action_index() {
+        
+        // load all post object from table that are software related
+        $postItems = ORM::factory('post')
+                ->where('id', '=', 0)
+                ->find();
         
         //detect device
         $browser = Request::user_agent('mobile');
         //http://kohanaframework.org/3.0/guide/api/Request - UserAgent Detection Here
-
         //If $browser is not null then device is mobile
-        if ($browser != null) {
+        if ($browser != null)
+        {
             $mobileDevice = "Mobile :: ";
-        } else {
+        }
+        else
+        {
             $mobileDevice = "";
         }
-        
-        
+
+
         $aTitle = 'Software, Electronics, Music and all-round Geekery';
-        $mobileDevice = $mobileDevice.$aTitle;
-        
+        $mobileDevice = $mobileDevice . $aTitle;
+
         $this->template->title = View::bind_global('title', $aTitle);
         $this->template->title = View::bind_global('site_title', $mobileDevice);
         $this->template->content = View::factory('post/index');
-        $this->template->postItems = View::bind_global('postItems',$postItems);
+        $this->template->postItems = View::bind_global('postItems', $postItems);
     }
-} // End Post
+
+    public function action_electronics() {
+
+        // load all post object from table that are electronic related
+        $postItems = ORM::factory('post')
+                ->where('id', '=', 1)
+                ->find();
+        
+        //detect device
+        $browser = Request::user_agent('mobile');
+        //http://kohanaframework.org/3.0/guide/api/Request - UserAgent Detection Here
+        //If $browser is not null then device is mobile
+        if ($browser != null)
+        {
+            $mobileDevice = "Mobile :: ";
+        }
+        else
+        {
+            $mobileDevice = "";
+        }
+
+
+        $aTitle = 'Software, <b>Electronics</b>, Music and all-round Geekery';
+        $mobileDevice = $mobileDevice . $aTitle;
+
+        $this->template->title = View::bind_global('title', $aTitle);
+        $this->template->title = View::bind_global('site_title', $mobileDevice);
+        $this->template->content = View::factory('post/index');
+        $this->template->postItems = View::bind_global('postItems', $postItems);
+    }
+
+}
+
+// End Post
