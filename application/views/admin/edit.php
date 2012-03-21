10676785e707fb3f2fd35fedc97424b661cb5ea5
David Power <david.lpower@gmail.com>
2012-03-21 00:06:45 +0000

    adding the type to the posts

--- application/views/admin/edit.php
+++ application/views/admin/edit.php
@@ -1,11 +1,11 @@
 <script type="text/javascript" src="/media/tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
 <script type="text/javascript">
-tinyMCE.init({
+    tinyMCE.init({
         theme : "advanced",
         mode : "textareas",
         plugins : "fullpage",
         theme_advanced_buttons3_add : "fullpage"
-});
+    });
 </script>
 
 <div id="container">
@@ -13,19 +13,24 @@ tinyMCE.init({
         <div class="centerTitle">
             <h1><?php echo($title) ?></h1>
         </div>
-        <?php include 'application/views/template/menuBar.php';?>
-        </div>
-        <br/>
-        <?php echo Form::open('admin/post/' . $post->id); ?>
-        <?php echo Form::label("title", "Title"); ?>
-        <br/>
-        <?php echo Form::input("title", $post->title, $settings=array("style='height:35px; width: 80%; font-size: large;'")); ?>
-        <br/>
-        <br/>
-        <?php echo Form::label("content", "Content"); ?>
-        <?php echo Form::textarea("content", $post->content, $settings=array("style='width: 80%; height: 400px'")); ?>
-        <br/>
-        <br/>
-        <?php echo Form::submit("submit", "Submit"); ?>
-        <?php echo Form::close(); ?>
+        <?php include 'application/views/template/menuBar.php'; ?>
+    </div>
+    <br/>
+    <?php echo Form::open('admin/post/' . $post->id); ?>
+    <?php echo Form::label("title", "Title"); ?>
+    <br/>
+    <?php
+        if(!isset($post)){
+            Form::label("type", "Type");
+        }   Form::input("type", $post->type);
+    ?>
+    <?php echo Form::input("title", $post->title, $settings = array("style='height:35px; width: 80%; font-size: large;'")); ?>
+    <br/>
+    <br/>
+    <?php echo Form::label("content", "Content"); ?>
+    <?php echo Form::textarea("content", $post->content, $settings = array("style='width: 80%; height: 400px'")); ?>
+    <br/>
+    <br/>
+    <?php echo Form::submit("submit", "Submit"); ?>
+    <?php echo Form::close(); ?>
 </div>
