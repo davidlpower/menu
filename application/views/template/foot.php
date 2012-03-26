d159c85390c5560b60d9ccbe6fcab72dc01ba410
David Power <david.lpower@gmail.com>
2012-03-26 23:31:33 +0100

    changing the location of the blog file

---
+++ application/views/template/foot.php
@@ -0,0 +1,9 @@
+
+        <div class="footer">
+            &copy;David Power
+            <div class="fb-like" data-href="http://www.karujahundu.com/" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-font="segoe ui"></div>
+        </div>
+
+
+    </body>  
+</html>
