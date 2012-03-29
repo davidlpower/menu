887d1e40bfe0078bf62675d9f5f48baf9958232b
David Power <david.lpower@gmail.com>
2012-03-29 14:16:08 +0100

    Massive update! 2

--- application/views/post/index.php
+++ application/views/post/index.php
@@ -1,27 +1,3 @@
-<div id="container">
-
-    <div id="title">
-
-        <div class="centerTitle">
-            <h1><?php echo($title) ?></h1>
-        </div>
-
-        <!--Display new menu-->
-        <div id="menuBar">
-            <div class="centerTitle">
-                <!-- Add a link to the post page. -->
-                <?php echo HTML::anchor("/", "Software"); ?>
-                <?php echo " | " ?>
-                <?php echo HTML::anchor("/electronics", "Electronics"); ?>
-                <?php echo " | " ?>
-                <?php echo HTML::anchor("/music", "Music"); ?>
-                <?php echo " | " ?>
-                <?php echo HTML::anchor("/everything_else", "Everything Else"); ?>
-                <?php echo " | " ?>
-                <?php echo HTML::anchor("/contact", "Contact"); ?>
-            </div>
-        </div>
-    </div>
 
     <?php foreach ($postItems as $post) : ?>
         <div class="menuItem">
@@ -34,4 +10,3 @@
 
     <?php endforeach; ?>
 
-</div>
