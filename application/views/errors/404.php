9816f1ffaf05d6e41599a143e0e08fbf90b40fd4
David Power <david.lpower@gmail.com>
2012-03-29 14:37:28 +0100

    Massive update! 3

--- application/views/errors/404.php
+++ application/views/errors/404.php
@@ -1,15 +1,7 @@
-<div id="container">
 
-    <div id="title">
-
-        <div class="centerTitle">
-            <h1><?php echo($title) ?></h1>
-        </div>
-    </div>
     <center>
         <h1>What have you done?!</h1>
         <p>I suggest you start again 
         <?php echo HTML::anchor('/','Click Me!') ?>
         </p>
     </center>
-</div>
