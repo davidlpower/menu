a4192e9e3628f33011fc58695c9ba4ba65faf250
David Power <david.lpower@gmail.com>
2012-03-30 22:14:58 +0100

    updated the footer style by 2%

--- application/views/post/index.php
+++ application/views/post/index.php
@@ -1,12 +1,13 @@
 
-    <?php foreach ($postItems as $post) : ?>
-        <div class="menuItem">
-            <h2><?php echo $post['title']; ?></h2>
-            <?php echo $post['content']; ?>
-            <div class="toRight">
-                <?php echo $post['dateAdded']; ?>
-            </div>
-        </div>
+<?php foreach ($postItems as $post) : ?>
 
-    <?php endforeach; ?>
+    <div class="menuItem">
+        <h2><?php echo $post['title']; ?></h2>
+        <?php echo $post['content']; ?>
+        <div class="toRight">
+            <?php echo $post['dateAdded']; ?>
+        </div>
+    </div>
+
+<?php endforeach; ?>
 
