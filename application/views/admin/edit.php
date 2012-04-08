82286335df7600d5936c290a79095b138a660499
David Power <david.lpower@gmail.com>
2012-04-08 17:21:41 +0100

    adding object_array

--- application/views/admin/edit.php
+++ application/views/admin/edit.php
@@ -8,27 +8,33 @@
     });
 </script>
 
-<?php Blog::debugPrint('id: ' . $category['id'] . ' Cat:' . $category['category'])
-        ?>
+<?php
+echo 'id: ';
+echo$category['id'];
+echo ' Cat:';
+echo $category['category'];
+die;
+?>
 
-    <br/>
-    <?php echo Form::open('admin/post/' . $post->id); ?>
-    <?php echo Form::label("title", "Title"); ?>
-    <br/>
-    <?php
-        if($post->type == null){
-            Form::label("type", "Type");
-        }   Form::input("type", $post->type);
-    ?>
-    <?php echo Form::input("title", $post->title, $settings = array("style='height:35px; width: 80%; font-size: large;'")); ?>
-    <br/>
-    <br/>
-    <?php echo Form::label("content", "Content"); ?>
-    <?php echo Form::textarea("content", $post->content, $settings = array("style='width: 80%; height: 400px'")); ?>
-    <br/>
-    <br/>
-    <?php echo Form::label("category", "Category"); ?>
-    <?php echo Form::select("category", $category['category']); ?><br/>
-    <br/>
-    <?php echo Form::submit("submit", "Submit"); ?>
-    <?php echo Form::close(); ?>
+<br/>
+<?php echo Form::open('admin/post/' . $post->id); ?>
+<?php echo Form::label("title", "Title"); ?>
+<br/>
+<?php
+if ($post->type == null)
+{
+    Form::label("type", "Type");
+} Form::input("type", $post->type);
+?>
+<?php echo Form::input("title", $post->title, $settings = array("style='height:35px; width: 80%; font-size: large;'")); ?>
+<br/>
+<br/>
+<?php echo Form::label("content", "Content"); ?>
+<?php echo Form::textarea("content", $post->content, $settings = array("style='width: 80%; height: 400px'")); ?>
+<br/>
+<br/>
+<?php echo Form::label("category", "Category"); ?>
+<?php echo Form::select("category", $category['category']); ?><br/>
+<br/>
+<?php echo Form::submit("submit", "Submit"); ?>
+<?php echo Form::close(); ?>
