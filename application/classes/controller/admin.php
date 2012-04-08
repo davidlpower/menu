c53508696667d1be90e6b5a8fbe69957b68ff4e4
David Power <david.lpower@gmail.com>
2012-04-08 16:10:48 +0100

    adding new category's

--- application/classes/controller/admin.php
+++ application/classes/controller/admin.php
@@ -5,15 +5,14 @@ defined('SYSPATH') or die('No direct script access.');
 class Controller_Admin extends Controller_Temp {
 
     const INDEX_PAGE = '/admin';
-
     public $mobileDevice = null;
 
     public function action_index() {
+        
         $postItems = ORM::factory('post')->find_all(); // load all admin object from table
         //detect device
         $browser = Request::user_agent('mobile');
-        //http://kohanaframework.org/3.0/guide/api/Request - UserAgent Detection Here
-        //If $browser is not null then device is mobile
+        
         if ($browser != null)
         {
             $mobileDevice = "Mobile Mode";
@@ -31,11 +30,9 @@ class Controller_Admin extends Controller_Temp {
 
     // loads the new article form
     public function action_new() {
+        
         $post = new Model_post();
-        
         $categories = $post->all_categories();
-        
-        
 
         $aTitle = 'Edit or Post something new!';
         View::bind_global('title', $aTitle);
@@ -46,6 +43,7 @@ class Controller_Admin extends Controller_Temp {
 
     // save the article
     public function action_post() {
+        
         $post_id = $this->request->param('id');
         $post = new Model_post($post_id);
         $post->values($_POST); // populate $post object from $_post array
@@ -56,6 +54,7 @@ class Controller_Admin extends Controller_Temp {
 
     // edit the admin items
     public function action_edit() {
+        
         $post_id = $this->request->param('id');
         $post = new Model_post($post_id);
         $aTitle = 'Edit that post!';
@@ -66,6 +65,7 @@ class Controller_Admin extends Controller_Temp {
 
     // delete the post item
     public function action_delete() {
+        
         $post_id = $this->request->param('id');
         $post = new Model_post($post_id);
 
