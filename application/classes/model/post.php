d625bfee7a0829b9886b88a183793de63193984d
David Power <david.lpower@gmail.com>
2012-04-08 17:19:44 +0100

    adding object_array

--- application/classes/model/post.php
+++ application/classes/model/post.php
@@ -1,18 +1,19 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php
+
+defined('SYSPATH') or die('No direct script access.');
 
 //Pulls the Menu table out of the database and provides a way for the system to intergrate with it.
 class Model_Post extends ORM {
-    
-    public function all_posts(){
-        
+
+    public function all_posts() {
+
         $results = DB::select()->from('posts')->as_object()->execute();
         return $results;
     }
-    
-    public function all_categories(){
-        
-        $results = DB::select('id','category')->from('post_category')->as_object()->execute();
 
+    public function all_categories() {
+
+        $results = DB::select()->from('post_category')->as_object()->execute();
         return $results;
     }
 
