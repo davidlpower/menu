03701a69422c684516212c4a375281c390c105dc
David Power <david.lpower@gmail.com>
2012-04-15 18:21:21 +0100

    adding google analytics

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -8,6 +8,20 @@
             echo HTML::style($file, array('media' => $type)), "\n"
             ?>
 
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-30906667-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
+    </head>
+
+    <body>
         <div id="fb-root"></div>
         <script>(function(d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
@@ -17,15 +31,15 @@
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
         </script>
-    </head>
-
-    <body>
+        
         <div id="container">
 
             <?php echo $head ?>
             <?php echo $content ?>
-            
+
         </div>
+        
         <?php echo $foot ?>
+        
     </body>  
 </html>
