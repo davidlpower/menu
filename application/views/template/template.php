6482158845f8dda2f83961eb0878e5a4d98e7b79
David Power <david.lpower@gmail.com>
2012-04-23 18:40:27 +0100

    Finishing published option.

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -2,44 +2,46 @@
     <head profile="http://gmpg.org/xfn/11">
         <title><?php echo $site_title ?></title>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <meta name="keywords" content="david, power, david power, limerick, Ireland, music, electronics, cooking, programming, software, arduino"> 
+        <meta name="description" content="David Power's personal blog."> 
 
-        <?php
-        foreach ($styles as $file => $type)
-            echo HTML::style($file, array('media' => $type)), "\n"
-            ?>
+                <?php
+                foreach ($styles as $file => $type)
+                    echo HTML::style($file, array('media' => $type)), "\n"
+                    ?>
 
-        <script type="text/javascript">
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-30906667-1']);
-            _gaq.push(['_trackPageview']);
+                <script type="text/javascript">
+                    var _gaq = _gaq || [];
+                    _gaq.push(['_setAccount', 'UA-30906667-1']);
+                    _gaq.push(['_trackPageview']);
 
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-        </script>
-    </head>
+                    (function() {
+                        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+                    })();
+                </script>
+                </head>
 
-    <body>
-        <div id="fb-root"></div>
-        <script>(function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-        </script>
-        
-        <div id="container">
+                <body>
+                    <div id="fb-root"></div>
+                    <script>(function(d, s, id) {
+                        var js, fjs = d.getElementsByTagName(s)[0];
+                        if (d.getElementById(id)) return;
+                        js = d.createElement(s); js.id = id;
+                        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));
+                    </script>
 
-            <?php echo $head ?>
-            <?php echo $content ?>
+                    <div id="container">
 
-        </div>
-        
-        <?php echo $foot ?>
-        
-    </body>  
-</html>
+                        <?php echo $head ?>
+                        <?php echo $content ?>
+
+                    </div>
+
+                    <?php echo $foot ?>
+
+                </body>  
+                </html>
