34822151938691ac640eb66787b7c808a8e01cd7
David Power <david@karujahundu.com>
2012-12-06 19:21:11 +0000

    i dont know!

--- application/classes/Blog.php
+++ application/classes/Blog.php
@@ -1,11 +1,15 @@
 <?php
-class Helper
+class Blog
 {
 
     public static function debugPrint($to_Prnt) {
         echo "<pre>";
         print_r($to_Prnt);
         die;
+    }
+    
+    public static function printMessage($to_Prnt) {
+        echo $to_Prnt;
     }
 }
 ?>
