afeb36d18f3ac25e7980ee85a9f778425b06d8dc
David Power <david@karujahundu.com>
2012-12-06 20:08:08 +0000

    give div name

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,6 +1,6 @@
 <div id="login">
     <form action="/login" method="post" accept-charset="utf-8">
-        <div id="">
+        <div id="login_form">
             <span id="">Username <input type="text" name="username"><span
             <span id="">Password <input type="password" name="password"></span>
             <input type="submit" name="submit" value="Login">    
