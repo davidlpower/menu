3b561d8af12cf4aec838ed431a6fb8ab5267bf45
David Power <david@karujahundu.com>
2012-12-06 20:13:10 +0000

    update look of login screen

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,9 +1,9 @@
 <div id="login">
     <form action="/login" method="post" accept-charset="utf-8">
-        <div id="login_form">
+        <div id="login_form" style=" margin-left: 14px; margin-top: 8px; ">
             <span id="">Username <input type="text" name="username"><span
-            <span id="">Password <input type="password" name="password"></span>
-            <input type="submit" name="submit" value="Login">    
+            <span id="">Password <input type="password" name="password" style=" margin-left: 5px; "></span>
+            <input type="submit" name="submit" value="Login" style=" margin-left: 80px; ">    
         </div>
     </form>
 </div>
